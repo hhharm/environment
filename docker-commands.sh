3568c9ec325d8c7bd14dd680a66acd2cd3841894
hhharm <natal_tolok@mail.ru>
2020-08-27 08:58:01 +0300

    Create docker-commands.sh

---
+++ docker-commands.sh
@@ -0,0 +1,19 @@
+docker container ls -a
+docker container start CONTAINERID
+docker exec -it CONTAINERID //bin//bash
+#(//bin//bash in git for windows, /bin/bash in powershell)
+
+docker image ls
+docker run NAME:TAG
+
+docker container stop CONTAINERID
+docker container rm CONTAINERID
+
+ping host.docker.internal
+(use host.docker.internal instead of localhost)
+
+#(if npm ci hangs inside docker - use PowerShell!)
+
+
+docker image build -t NAME:TAG --file=FILESDIR .\
+docker run --name=kreader1_5 -d -v  FILESDIR\:app -p 9230:9230 -p 4004:4004 NAME:TAG
